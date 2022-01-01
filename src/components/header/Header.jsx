daefeb0c6511da4a495402e2c41472169887f045
Mukul Patel <Mukul215@gmail.com>
2022-01-01 11:34:56 -0500

    added header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,21 @@
+import logo from '../../assets/meme-face.png'
+
+const Header = () => {
+  return (
+    <div className='flex h-20 items-center bg-gradient-to-r from-[#4C1461] via-[#701A8E] to-[#7F07A9]'>
+      <div className='absolute left-10 flex'>
+        <div>
+          <img src={logo} alt='logo' className='w-10 pr-1'/>
+        </div>
+        <div className='pl-1'>
+          <p className='text-white text-xl'>Meme Generator</p>
+        </div>
+      </div>
+      <div className='absolute right-10'>
+        <p className='text-white text-sm'>React Course - Project 3</p>
+      </div>
+    </div>
+  )
+}
+
+export default Header
